28202075d1495699b9ce91ce65bd35952ff9fd8d
Youssef <youssefyaser2020@gmail.com>
2025-07-13 23:42:33 +0300

    refactor: remove commented-out getter for imagePath in UserComponent

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -14,10 +14,6 @@ export class UserComponent {
 
   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
-
   onSelectedUser() {
     const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser.set(DUMMY_USERS[randomNumber]);
