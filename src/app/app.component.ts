79e664ca837bbd35b7876d55d5617f0c56dcab11
Youssef <youssefyaser2020@gmail.com>
2025-07-15 21:49:34 +0300

    feat: initialize users with DUMMY_USERS in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { UserComponent } from "./components/user/user.component";
+import { UserComponent } from './components/user/user.component';
+
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,5 @@ import { UserComponent } from "./components/user/user.component";
 })
 export class AppComponent {
   title = 'EasyTrack';
+  users = DUMMY_USERS;
 }
