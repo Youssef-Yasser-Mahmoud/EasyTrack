2815ffe6f319902bf6075c513315c8654dad3570
Youssef <youssefyaser2020@gmail.com>
2025-07-16 17:01:17 +0300

    refactor: enhance UserComponent by adding EventEmitter for user selection and updating imagePath logic

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,8 +1,12 @@
-import { Component, computed, Input, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
 
-import { DUMMY_USERS } from '../../dummy-users';
-
-const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   imports: [],
@@ -10,21 +14,22 @@ const randomNumber = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) id!: string;
-  // @Input({ required: true }) name!: string;
-  // @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Output() select = new EventEmitter<string>();
 
-  id = input.required<string>();
-  name = input.required<string>();
-  avatar = input.required<string>();
+  // id = input.required<string>();
+  // name = input.required<string>();
+  // avatar = input.required<string>();
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar();
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
-  imagePath = computed(() => 'assets/users/' + this.avatar());
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
 
   onSelectedUser() {
-    // this.id.set() // Error because signals inputs are read-only
+    this.select.emit(this.id);
   }
 }
