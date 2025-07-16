4c34d7aaa211be392f1be45a6c6330ef9fad11aa
Youssef <youssefyaser2020@gmail.com>
2025-07-16 21:20:31 +0300

    feat: integrate TasksComponent into AppComponent and update user selection method to store user name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
 
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./components/tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'EasyTrack';
   users = DUMMY_USERS;
+  userName = '';
 
-  onReceiveUser(id: string) {
-    console.log('selected id' + id);
+  onReceiveUser(name: string) {
+    console.log('selected name' + name);
+    this.userName = name;
   }
 }
