ce98bd611d4a2143a4c5fc77b8a48407cb8a742b
Youssef <youssefyaser2020@gmail.com>
2025-07-19 22:55:53 +0300

    refactor: update user component to use a single user input object and improve template bindings

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,6 +8,12 @@ import {
   Output,
 } from '@angular/core';
 
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -15,14 +21,7 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) id!: string;
-  // @Input({ required: true }) name!: string;
-  // @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) user!: {
-    id: string,
-    name: string,
-    avatar: string
-  }
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
